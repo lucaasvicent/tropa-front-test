08d2f9de6ecbac0e3f06acb64522885125d46cd1
Lucas Vicente <lucasdev08@hotmail.com>
2025-06-09 12:14:44 -0300

    feat: partial completion of the project

--- src/app/(protected)/event/components/index.tsx
+++ src/app/(protected)/event/components/index.tsx
@@ -1,9 +1,54 @@
-import { ComponentContent, Container } from "./styles";
+import { Plus } from "phosphor-react";
+import { Container, ComponentContent, Header, SearchInput, InsertButton, Table, TableRow, TableHeader, TableCell, StatusDot, Pagination, SearchContainer, SearchIcon } from "./styles";
 
 export default function EventDashboard() {
   return (
     <Container>
       <ComponentContent>
+        <Header>
+          <SearchContainer>
+            <SearchIcon size={16} weight="bold" />
+            <SearchInput placeholder="Buscar eventos" />
+          </SearchContainer>
+          <InsertButton><Plus size={16} weight="bold" /> Inserir novo</InsertButton>
+        </Header>
+
+
+        <Table>
+          <thead>
+            <TableRow>
+              <TableHeader>Nome do evento</TableHeader>
+              <TableHeader>Total de equipes</TableHeader>
+              <TableHeader>Status</TableHeader>
+              <TableHeader>Data</TableHeader>
+              <TableHeader></TableHeader>
+            </TableRow>
+          </thead>
+          <tbody>
+            <TableRow>
+              <TableCell>Clube do Laço Coração Pantaneiro</TableCell>
+              <TableCell>10</TableCell>
+              <TableCell><StatusDot /> Ativo</TableCell>
+              <TableCell>09 a 11 de Junho</TableCell>
+              <TableCell>⋮</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Clube do Laço Coração Pantaneiro</TableCell>
+              <TableCell>10</TableCell>
+              <TableCell><StatusDot /> Ativo</TableCell>
+              <TableCell>09 a 11 de Junho</TableCell>
+              <TableCell>⋮</TableCell>
+            </TableRow>
+          </tbody>
+        </Table>
+
+        <Pagination>
+          <button>Anterior</button>
+          <button className="active">1</button>
+          <button>2</button>
+          <button>3</button>
+          <button className="active">Próxima</button>
+        </Pagination>
       </ComponentContent>
     </Container>
   );
--- src/app/(protected)/event/components/styles.ts
+++ src/app/(protected)/event/components/styles.ts
@@ -1,17 +1,113 @@
+import { MagnifyingGlass } from "phosphor-react";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 74rem;
-  height: 18.06rem;
+  width: 93%;
   background: var(--white-100);
   border-radius: 0.625rem;
-  justify-content: center;
   display: flex;
+  justify-content: center;
   padding: 1.25rem;
+  height: 15.06rem;
+  align-items: center;
+  font-family: Roboto, sans-serif;
 `;
 
 export const ComponentContent = styled.div`
-  width: 1170px;
+  width: 100%;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-bottom: 1rem;
+  gap: 4rem;
+`;
+
+export const SearchContainer = styled.div`
+  position: relative;
+  width: 200px;
+`;
+
+export const SearchInput = styled.input`
+  padding: 0.5rem 1rem 0.5rem 2.25rem; 
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  width: 100%;
+`;
+
+export const SearchIcon = styled(MagnifyingGlass)`
+  position: absolute;
+  top: 50%;
+  left: 0.75rem;
+  transform: translateY(-50%);
+  color: #999;
 `;
 
 
+export const InsertButton = styled.button`
+  background-color: #d05c2e;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  font-size: 0.875rem;
+`;
+
+export const TableRow = styled.tr`
+  border-bottom: 1px solid #eee;
+`;
+
+export const TableHeader = styled.th`
+  text-align: left;
+  padding: 0.75rem 0;
+  color: var(--orange-200);
+`;
+
+export const TableCell = styled.td`
+  padding: 0.75rem 0;
+  vertical-align: middle;
+  color: var(--gray-100);
+`;
+
+export const StatusDot = styled.span`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: #2cd03d;
+  border-radius: 50%;
+  margin-right: 0.5rem;
+`;
+
+export const Pagination = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 1rem;
+
+  button {
+    background: #fff;
+    border: 1px solid #ccc;
+    margin-left: 0.25rem;
+    padding: 0.5rem 1rem;
+    border-radius: 100px;
+    cursor: pointer;
+
+    &.active {
+      background: #d05c2e;
+      color: white;
+      border: none;
+    }
+  }
+`;
